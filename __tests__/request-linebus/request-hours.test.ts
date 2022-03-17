74745bb7307cfcfb28a8cf59e577664438dac74d
ValimGustavo <gustavo.souza.valim@gmail.com>
2022-03-16 23:13:18 -0300

    wip: added get start point by lineBus

--- __tests__/request-linebus/request-hours.test.ts
+++ __tests__/request-linebus/request-hours.test.ts
@@ -6,23 +6,22 @@ import { ItineraryService } from "../../src/services/Itinerary.service";
 describe('get next hour', () => {
   test('get hour 05:27 to lineBus 123456 in start point A', async () => {
     const hour = new Date('2022-03-12T05:26:00');
-    const dailyHour = 'DIAS ÚTEIS';
+    const dailyWeek = 'DIAS ÚTEIS';
     const startPoint = "Saída PONTO A";
 
     const lineBusTest = await getLineBus('123456') as LineBus
 
     const itineraryService = new ItineraryService(lineBusTest);
-    const timer = itineraryService.setHoursIfNotHave({ hour, dailyHour });
-    const itinerary = itineraryService.getItinerary(startPoint);
+    const timer = itineraryService.setHoursIfNotHave({ hour, dailyWeek });
 
-    const nextHour = itineraryService.nextHour(itinerary, timer)
+    const nextHour = itineraryService.nextHour(startPoint, timer)
 
     expect(nextHour).toBe('05:27')
   })
 
   test('get hour not found to lineBus 123456 in start point A', async () => {
     const hour = new Date('2022-03-12T23:59:00');
-    const dailyHour = 'DIAS ÚTEIS';
+    const dailyWeek = 'DIAS ÚTEIS';
     const startPoint = "Saída PONTO A";
 
 
@@ -30,10 +29,10 @@ describe('get next hour', () => {
 
 
     const itineraryService = new ItineraryService(lineBusTest);
-    const timer = itineraryService.setHoursIfNotHave({ hour, dailyHour });
+    const timer = itineraryService.setHoursIfNotHave({ hour, dailyWeek });
     const itinerary = itineraryService.getItinerary(startPoint);
 
-    expect(() => itineraryService.nextHour(itinerary, timer)).toThrowError('hour not found to itinerary');
+    expect(() => itineraryService.nextHour(startPoint, timer)).toThrowError('hour not found to itinerary');
   })
 
   test('get itinerary not found to lineBus 123456 because start point C not exist', async () => {
@@ -63,6 +62,16 @@ describe('get next hour', () => {
     }
 
     expect(expectDate).toEqual(timerExpect);
+  })
+
+  test('get start points to lineBus 123456', async () => {
+    const lineBusTest = await getLineBus('123456') as LineBus
+
+    const itineraryService = new ItineraryService(lineBusTest);
+    const startPoints = itineraryService.getStartPoints();
+
+    expect(startPoints[0]).toEqual("Saída PONTO A");
+    expect(startPoints[1]).toEqual("Saída PONTO B");
   })
 })
 
